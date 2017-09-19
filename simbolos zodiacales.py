6ca8c4349c233ce629f09387d613ce5049f1c6d7
FrMiMoAl <32112849+FrMiMoAl@users.noreply.github.com>
2017-09-19 18:12:28 -0400

    add zodiaco.py

---
+++ simbolos zodiacales.py
@@ -0,0 +1,23 @@
+dia=input("Dia")
+mes=input("Mes")
+if dia>=1 and <=31;
+   print("el dia esta correcto")
+   if mes>1=1 and <=12;
+   print("el mes esta correcto/n/n")
+   #ambos datos estan bien
+   #20/01 al 18/02
+   if (mes == 1 and dia >20)or(mes == 18 and dia <=02 ):
+       print ("el signo es Acuario")
+   #19/02 al 20/03
+   if (mes == 02 and dia >19)or(mes == 3 and dia <=20 ):
+       print ("el signo es Piscis")
+   #21/03 al 20/04
+   if (mes == 3 and dia >21)or(mes == 4 and dia <=20 ):
+       print ("el signo es Aries")
+   #21/03 al 20/04
+   if (mes == 3 and dia >21)or(mes == 4 and dia <=20 ):
+       print ("el signo es Aries")
+    else :
+        print("Error en el mes")
+ else:
+    print("Error en el dia")
